7e4e77f143ae82e9a0c8f95c903e246a84a0c452
marticha09 <hokinmartin590@gmail.com>
2025-01-27 14:09:38 +0200

    Add files via upload

---
+++ imena/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kak se kazvash");
+            int n = int.Parse(Console.ReadLine());
+           // int[] arr = new int[n];
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+
+            }
+            int[] numbers = new int[10];
+            Console.WriteLine("kolko ocenki imash");
+
+           
+        }
+    }
+}
