2c8cf88f1bf88a26633f2bae368cc109e04e1923
marticha09 <hokinmartin590@gmail.com>
2025-04-09 13:44:56 +0300

    Add files via upload

---
+++ MASIVI/Program.cs
@@ -0,0 +1,32 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASIVI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nomera = new int[5];
+            string[] imena = new string[5];
+            //vhod 1
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Vavedete nomera {i+1} :");
+                nomera[i] = int.Parse(Console.ReadLine());
+                Console.Write($"Vavedete ime {i} :");
+                 imena[i] =(Console.ReadLine());
+                
+            }
+
+            Console.WriteLine(string.Join(", ", nomera));
+            Console.WriteLine(string.Join(", ", imena));
+            
+
+
+        }
+    }
+}
